735f9a4ed57a9491bec04606d823f56f854a00e2
Dane Merriman <merriman.dane02@gmail.com>
2022-03-20 20:53:17 -0400

    First commit

---
+++ Source.cpp
@@ -0,0 +1,15 @@
+// Dane Merriman
+// CIS 1202 501
+// March 20, 2022
+// Ninth Assignment - Source Code Management 1
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Hello, World!" << endl;
+
+	system("pause");
+	return 0;
+}
